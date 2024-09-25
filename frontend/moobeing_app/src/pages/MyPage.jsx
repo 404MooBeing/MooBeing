ed03df9589d92eccbae44371b347d005c16e03a1
jongho <whdghthdms44@gmail.com>
2024-09-25 15:20:35 +0900

    feat: My Page 기본 구현

--- frontend/moobeing_app/src/pages/MyPage.jsx
+++ frontend/moobeing_app/src/pages/MyPage.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import styled from "styled-components";
+import MyInfo from "../components/User/MyInfo";
+import PasswordChange from "../components/User/PasswordChange";
+import Header from "../components/Fixed/Header";
+import Footer from "../components/Fixed/Footer";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  overflow-y: auto;
+  box-sizing: border-box;
+`;
+
+const MainContent = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
 const MyPage = () => {
-    return (
-        <div>
-            <h1>마이페이지</h1>
-        </div>
-    );
+  const [isPasswordChangeVisible, setIsPasswordChangeVisible] = useState(false);
+
+  const handlePasswordChangeClick = () => {
+    setIsPasswordChangeVisible(true);
+  };
+
+  return (
+    <Container>
+      <MainContent>
+        {!isPasswordChangeVisible ? (
+          <MyInfo onPasswordChangeClick={handlePasswordChangeClick} />
+        ) : (
+          <PasswordChange />
+        )}
+      </MainContent>
+    </Container>
+  );
 };
 
 export default MyPage;
