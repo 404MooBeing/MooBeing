833a998bc939b65b30664060cc43af5016bf08a2
jongho <whdghthdms44@gmail.com>
2024-09-27 16:31:37 +0900

    feat: Coin Page, Coin Exchange Page 뼈대 구현

---
+++ frontend/moobeing_app/src/pages/CoinPage.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import styled from "styled-components";
+import CoinInfo from "../components/Coin/CoinInfo";
+import CoinList from "../components/Coin/CoinList";
+const Screen = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh; /* 전체 화면을 차지 */
+`;
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  overflow-y: auto; /* 내부 콘텐츠만 스크롤 가능 */
+  box-sizing: border-box;
+  padding-bottom: 150px; /* Footer 공간 확보 */
+`;
+
+const CoinPage = () => {
+
+   // 조회 조건 관리
+  const [sortCriteria, setSortCriteria] = useState({ period: "1개월", type: "전체" });
+  
+  // 정렬 기준 변경 함수
+  const handleSortSelect = (selectedSort) => {
+    setSortCriteria(selectedSort); // 정렬 기준 업데이트
+  };
+
+  return (
+    <Screen>
+      <Container>
+        무코인
+        <CoinInfo
+          onSortSelect={handleSortSelect}
+          sortCriteria={sortCriteria}
+        />
+        <CoinList 
+          sortCriteria={sortCriteria} 
+        />
+      </Container>
+    </Screen>
+  );
+};
+
+export default CoinPage;
