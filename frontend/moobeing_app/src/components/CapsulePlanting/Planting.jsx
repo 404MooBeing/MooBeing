bca0fa92f7d5b19db519e47cdf003763bc9c5ca3
thunthuni1004 <thunthuni1004@gmail.com>
2024-09-30 13:26:13 +0900

    feat: 무캡슐 심어진 결과 로딩페이지
    - 심어지는 로딩창 출력 4초 후 나옴
    - 날짜 api 연결해야 함

---
+++ frontend/moobeing_app/src/components/CapsulePlanting/Planting.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 40px;
+  text-align: center;
+  margin-top: 30%;
+`;
+
+function Planting() {
+  return (
+    <Title>
+      무가 심어지고 <br /> 있습니다.
+    </Title>
+  );
+}
+
+export default Planting;
