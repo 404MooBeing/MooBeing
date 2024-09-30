4f00e03965f289fe21ecb912986a97b1b98fa3d9
조승기 <beenzino@kookmin.ac.kr>
2024-09-30 11:37:15 +0900

    fix: AccountProduct 테이블명 명시

--- backend/moobeing/src/main/java/com/im/moobeing/domain/account/entity/AccountProduct.java
+++ backend/moobeing/src/main/java/com/im/moobeing/domain/account/entity/AccountProduct.java
@@ -1,9 +1,6 @@
 package com.im.moobeing.domain.account.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -11,6 +8,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Table(name = "Account_Product")
 public class AccountProduct {
     @Id
     @GeneratedValue
