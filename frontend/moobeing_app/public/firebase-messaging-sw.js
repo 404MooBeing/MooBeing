622364e9375f1b35c2abb4cd6c12719b03dd99d8
chwangmin <ckm7907@naver.com>
2024-10-08 15:08:16 +0900

    Test: 중복 보네기 로직 변경

--- frontend/moobeing_app/public/firebase-messaging-sw.js
+++ frontend/moobeing_app/public/firebase-messaging-sw.js
@@ -22,13 +22,24 @@ const messaging = firebase.messaging();
 
 // 중복된 알림 방지 로직 추가
 let lastNotificationData = null;
+let lastNotificationTimestamp = 0;
+const THROTTLE_TIME = 1000; // 알림 중복 제한 시간 (밀리초 단위)
 
+// background 메시지 처리
 messaging.onBackgroundMessage((payload) => {
   console.log("[firebase-messaging-sw.js] Received background message ", payload);
 
+  const currentTime = Date.now();
   const notificationData = JSON.stringify(payload.notification);
-  if (lastNotificationData === notificationData) return;
+
+  // 중복 확인: 같은 알림이고 설정된 시간 내일 경우 표시하지 않음
+  if (lastNotificationData === notificationData && (currentTime - lastNotificationTimestamp) < THROTTLE_TIME) {
+    return;
+  }
+
+  // 알림 표시 및 마지막 알림 정보 업데이트
   lastNotificationData = notificationData;
+  lastNotificationTimestamp = currentTime;
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
