1e51e028cf93a30c7db3a4f9a73b8564a62d076d
ParkSangwoo <psw1253@naver.com>
2024-10-08 12:46:56 +0900

    fix: [BE] WebSecurityPath에 /subscription/** 추가

--- backend/moobeing/src/main/java/com/im/moobeing/global/path/WebSecurityPath.java
+++ backend/moobeing/src/main/java/com/im/moobeing/global/path/WebSecurityPath.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum WebSecurityPath {
-    REQUIRE_AUTH_PATH("/user", "/user/pw", "/user/radish", "/quiz/**", "/user/select", "/credit/**", "/expense/**", "/loan/**","/account/**", "/user/baby", "/user/baby-merge", "/account/**", "/user/month-button", "/user/month-button-click", "/stream-cnt", "/user/streamCnt", "/radish/**", "/expense/**", "/alarm/**", "/point/**");
+    REQUIRE_AUTH_PATH("/subscription/**", "/user", "/user/pw", "/user/radish", "/quiz/**", "/user/select", "/credit/**", "/expense/**", "/loan/**","/account/**", "/user/baby", "/user/baby-merge", "/account/**", "/user/month-button", "/user/month-button-click", "/stream-cnt", "/user/streamCnt", "/radish/**", "/expense/**", "/alarm/**", "/point/**");
 
     private final String[] paths;
 
