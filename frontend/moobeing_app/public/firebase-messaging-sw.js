09d92958c284d4760650daa60a42a7dac0de50ed
chwangmin <ckm7907@naver.com>
2024-10-08 17:25:24 +0900

    github test firebase version 변경

--- frontend/moobeing_app/public/firebase-messaging-sw.js
+++ frontend/moobeing_app/public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 /* global importScripts, firebase */
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging.js');
 
 console.log('===========================================');
 console.log('FIREBASE SERVICE WORKER LOADED');
