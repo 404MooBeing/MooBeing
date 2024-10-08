1bdcb3f49c630ae851d2600c17b9b2912ce10e52
chwangmin <ckm7907@naver.com>
2024-10-08 16:49:33 +0900

    Test: github1:

--- frontend/moobeing_app/public/firebase-messaging-sw.js
+++ frontend/moobeing_app/public/firebase-messaging-sw.js
@@ -1,53 +1,94 @@
-// public/firebase-messaging-sw.js
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.5.0/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging-compat.js"
-);
+/* global importScripts, firebase */
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging.js');
 
-// Firebase 설정 객체 (firebase.ts와 동일한 값 사용)
-const firebaseConfig = {
-  apiKey: "AIzaSyCTHRWWL50vqyNfYQnJofsEItQxl2fgI3I",
-  authDomain: "moobeing-ee0eb.firebaseapp.com",
-  projectId: "moobeing-ee0eb",
-  storageBucket: "moobeing-ee0eb.appspot.com",
-  messagingSenderId: "830764540752",
-  appId: "1:830764540752:web:aa3e3215695f7b78288255",
-  measurementId: "G-2MW15Q9GCQ"
-};
+console.log('===========================================');
+console.log('FIREBASE SERVICE WORKER LOADED');
+console.log('===========================================');
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: '',
+  projectId: '',
+  messagingSenderId: '',
+  appId: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  measurementId: '',
+});
+
+class CustomPushEvent extends Event {
+  constructor(data) {
+    super('push');
+
+    Object.assign(this, data);
+    this.custom = true;
+  }
+}
+
+/*
+ * Overrides push notification data, to avoid having 'notification' key and firebase blocking
+ * the message handler from being called
+ */
+self.addEventListener('push', (e) => {
+  // Skip if event is our own custom event
+  if (e.custom) return;
+
+  // Kep old event data to override
+  const oldData = e.data;
+
+  // Create a new event to dispatch, pull values from notification key and put it in data key,
+  // and then remove notification key
+  const newEvent = new CustomPushEvent({
+    data: {
+      ehheh: oldData.json(),
+      json() {
+        const newData = oldData.json();
+        newData.data = {
+          ...newData.data,
+          ...newData.notification,
+        };
+        delete newData.notification;
+        return newData;
+      },
+    },
+    waitUntil: e.waitUntil.bind(e),
+  });
+
+  // Stop event propagation
+  e.stopImmediatePropagation();
+
+  // Dispatch the new wrapped event
+  dispatchEvent(newEvent);
+});
+
 const messaging = firebase.messaging();
 
-// 중복 알림을 방지하기 위한 Set
-const notificationIds = new Set();
-
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
+  // console.log('[firebase-messaging-sw.js] Received background message ', payload);
 
-  // 고유한 알림 ID 생성 (예: 타임스탬프 + 제목)
-  const notificationId = `${Date.now()}-${payload.notification.title}`;
+  const { title, body, icon, badge, ...restPayload } = payload.data;
 
-  // 이미 표시된 알림인지 확인
-  if (!notificationIds.has(notificationId)) {
-    notificationIds.add(notificationId);
+  const notificationOptions = {
+    body,
+    icon: icon || '/icons/firebase-logo.png', // path to your "fallback" firebase notification logo
+    badge: badge || '/icons/firebase-logo.png', // path to your "fallback" notification badge (Instead of the default bell)
+    data: restPayload,
+  };
 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-      icon: "/firebase-logo.png", // 원하는 아이콘으로 변경
-      tag: notificationId, // 알림에 고유 태그 추가
-    };
+  return self.registration.showNotification(title, notificationOptions);
+});
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+self.addEventListener('notificationclick', (event) => {
+  // console.log('[firebase-messaging-sw.js] notificationclick ', event);
 
-    // 일정 시간 후 Set에서 알림 ID 제거 (예: 10초 후)
-    setTimeout(() => {
-      notificationIds.delete(notificationId);
-    }, 1000);
+  // click_action described at https://github.com/BrunoS3D/firebase-messaging-sw.js#click-action
+  if (event.notification.data && event.notification.data.click_action) {
+    self.clients.openWindow(event.notification.data.click_action);
+  } else {
+    self.clients.openWindow(event.currentTarget.origin);
   }
+  
+  // close notification after click
+  event.notification.close();
 });
