f8efb2346edf5e2853326c64e39f7c4ed56e012b
wony0321 <lyw1212@gmail.com>
2024-10-08 09:30:05 +0900

    feat: home 수정 및 chatbot

--- frontend/moobeing_app/src/components/Home/NextRadishInfo.jsx
+++ frontend/moobeing_app/src/components/Home/NextRadishInfo.jsx
@@ -100,7 +100,7 @@ const RadishInfo = () => {
       const formattedDuration = `${days}일 ${hours}시간 ${minutes}분 ${remainingSeconds}초`;
       return formattedDuration;
     } else {
-      return "시간이 만료되었습니다!";
+      return "무를 뽑을 수 있습니다!";
     }
   };
 
