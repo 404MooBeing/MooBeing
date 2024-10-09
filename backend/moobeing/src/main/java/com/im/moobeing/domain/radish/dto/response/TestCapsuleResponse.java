e87423258b831e6b44bdb4b5efe9a9d62d90ba36
조승기 <beenzino@kookmin.ac.kr>
2024-10-09 15:43:52 +0900

    feat: 무심기 테스트 메서드 추가

---
+++ backend/moobeing/src/main/java/com/im/moobeing/domain/radish/dto/response/TestCapsuleResponse.java
@@ -0,0 +1,16 @@
+package com.im.moobeing.domain.radish.dto.response;
+
+import com.im.moobeing.domain.radish.entity.RadishCapsule;
+
+public record TestCapsuleResponse(
+        Long id,
+        String title,
+        String description,
+        String placeName,
+        String img,
+        boolean isHarvested
+) {
+    public static TestCapsuleResponse of(RadishCapsule radishCapsule) {
+        return new TestCapsuleResponse(radishCapsule.getId(), radishCapsule.getAddressName(), radishCapsule.getDescription(), radishCapsule.getDescription(), radishCapsule.getImgUrl(), radishCapsule.isHarvested());
+    }
+}
